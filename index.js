e914e014f1591653fcba6563794bac2735d92190
pp2837jp-official <85353418+tentoumushi-jp@users.noreply.github.com>
2021-11-03 14:22:26 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const sennanahyaku = 1700;
+var counting_push = 0;
+
+function count();{
+    counting_push = counting_push + 1;
+    check();
+}
+
+function check();{
+    if (sennanahyaku = 1700){
+        aleat("ﾄﾞｩﾜｧ!ｾﾝﾅﾅﾋｬｸｳ!");
+    };
+}
